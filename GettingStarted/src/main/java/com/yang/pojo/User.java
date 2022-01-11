4318d7d352d6cff94fe522cb0f523a10a869f14b
YangFighting <yangzhang_y@163.com>
2022-01-12 00:00:47 +0800

    【A】 ioc--基于xml方式注入属性

--- GettingStarted/src/main/java/com/yang/pojo/User.java
+++ GettingStarted/src/main/java/com/yang/pojo/User.java
@@ -11,6 +11,23 @@ import com.sun.org.slf4j.internal.LoggerFactory;
 public class User {
     private static final Logger logger = LoggerFactory.getLogger(User.class);
 
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void add() {
         logger.warn("add......");
     }
--- GettingStarted/src/test/java/com/yang/pojo/UserTest.java
+++ GettingStarted/src/test/java/com/yang/pojo/UserTest.java
@@ -53,5 +53,43 @@ public class UserTest {
 
     }
 
+    @Test
+    public void user1Test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        User user1 = context.getBean("user1", User.class);
+        Assert.assertNotNull(user1);
+        logger.warn(user1.toString());
+        logger.warn("name: " + user1.getName());
+    }
+
+    @Test
+    public void user2Test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        User user2 = context.getBean("user2", User.class);
+        Assert.assertNotNull(user2);
+        logger.warn(user2.toString());
+        logger.warn("name: " + user2.getName());
+
+        context = new ClassPathXmlApplicationContext("bean2.xml");
+        User user1 = context.getBean("user1", User.class);
+        Assert.assertNotNull(user1);
+        logger.warn(user1.toString());
+        logger.warn("name: " + user1.getName());
+
+    }
+
+    @Test
+    public void user3Test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        User user3 = context.getBean("user3", User.class);
+        Assert.assertNotNull(user3);
+        logger.warn(user3.toString());
+        logger.warn("name: " + user3.getName());
+
+
+    }
+
+
+
 
 }
