8a8048ebf14c64cee0d673654b93bd5045b12535
YangFighting <yangzhang_y@163.com>
2022-01-12 00:36:54 +0800

    【A】 ioc--注入属性——内部 bean && 级联赋值

--- GettingStarted/src/main/java/com/yang/service/UserService.java
+++ GettingStarted/src/main/java/com/yang/service/UserService.java
@@ -1,9 +1,11 @@
 package com.yang.service;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.yang.dao.Impl.UserDaoImpl;
 import com.yang.dao.UserDao;
+import com.yang.pojo.User;
 
 /**
  * @author zhangyang
@@ -14,12 +16,28 @@ public class UserService {
 
     private UserDao userDao;
 
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
 
-    public void add(){
+    public void add() {
         logger.warn("UserService add...");
         userDao.update();
+    }
+
+    public void getUserObject() {
+        logger.warn("UserService getUserName...");
+        logger.warn("user :" + getUser());
     }
 }
--- GettingStarted/src/test/java/com/yang/service/UserServiceTest.java
+++ GettingStarted/src/test/java/com/yang/service/UserServiceTest.java
@@ -1,7 +1,10 @@
 package com.yang.service;
 
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,14 +13,37 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @date 2022/01/11 23:56
  **/
 public class UserServiceTest {
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceTest.class);
 
     @Test
-    public void addTest(){
+    public void addTest() {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean3.xml");
         UserService userService = applicationContext.getBean("userService", UserService.class);
         Assert.assertNotNull(userService);
         userService.add();
+
+    }
+
+    @Test
+    public void getUserObjectTest() {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean3.xml");
+        UserService userService = applicationContext.getBean("userService1", UserService.class);
+        Assert.assertNotNull(userService);
+        userService.getUserObject();
+        logger.warn("user name: " + userService.getUser().getName());
+
+    }
+
+    @Test
+    public void getUserObject2Test() {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean3.xml");
+        UserService userService = applicationContext.getBean("userService2", UserService.class);
+        Assert.assertNotNull(userService);
+        userService.getUserObject();
+        logger.warn("user name: " + userService.getUser().getName());
 
     }
 }
